c2270585e1d80eedb5b0b8d605d7d1b41d46904b
shivam patra <spworld30@gmail.com>
2026-01-14 11:21:45 +0530

    added_files

---
+++ python/week1/loading_dataset.py
@@ -0,0 +1,3 @@
+import pandas as pd;
+df=pd.read_csv("f1.csv")
+print(df)
---
+++ python/week1/pandas_learn.py
@@ -0,0 +1,11 @@
+import pandas as pd;
+my_dataset={
+    "cars":["BMW","Tesla","Tata"],
+    "passing":[12,21,100],
+    "test":[0,0.9,1]
+}
+
+var = pd.DataFrame(my_dataset);
+print(var);
+
+
